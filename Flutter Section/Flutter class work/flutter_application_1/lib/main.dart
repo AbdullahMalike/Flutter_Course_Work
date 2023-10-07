19ff98463a14e8cacbef58668aed75ea17b82242
AbdullahMalike <abdullahmalikae2002@gmail.com>
2023-10-07 18:46:58 +0500

    FLutter work uploaded

---
+++ Flutter Section/Flutter class work/flutter_application_1/lib/main.dart
@@ -0,0 +1,549 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // esme home hmara widget hai ye app ka very first page ho ga
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("First App")),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.grey,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.brown,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightBlue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.lightGreen,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        drawer: Drawer(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
